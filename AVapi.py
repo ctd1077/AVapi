1b7c45246be14153b6b1a13d01008bf3ab7f3c37
Cam Deatherage <41445364+ctd1077@users.noreply.github.com>
2019-04-28 11:43:15 -0600

    File updated to match changes in jupyter notebook

--- AVapi.py
+++ AVapi.py
@@ -1,21 +1,37 @@
 #!/usr/bin/python3
 #Alpha Vatange API APP
-#Enter in stock symbol
-import plotly.plotly as py
-import plotly.figure_factory as ff
-import pandas as pd
+
 import requests
+import numpy as np
+import pandas as pd
 import json as js
 import matplotlib.pyplot as plt
 
-def fetch_symbol():
-    stock = ''
-    while not stock:
-        stock = input("What stock would you like to view? ")
-    return requests.get(api_root + api_series + stock + api).json()
+def fetch_symbol(stock):
+    return requests.get(api_root + api_series + stock + api, stream=False).json()
+
+def company_symbol():
+    try:
+        stc = ''
+        while not stc:
+            stc = input("What stock would you like to view? ")
+        stock = fetch_symbol(stc)
+        if len(stc) == 0:
+            print("Please enter a symbol.")
+        #elif len(stc).isdigit() == True:
+            #print ("It Works")
+        #elif stock != '"Meta Data"':
+            #print('elif statment')
+            # Takes to long. Waits for JSON file and doesn't ask
+            # to enter in another stock fetch_symbol
+            # qyuicker way to tell not a stock symbol?
+    except requests.exceptions.ConnectionError:
+        print("Couldn't connect to server! Please check the network?")
+    return stock
 
 def series():
-    """this function promts the user for the desired time series. Day, week, month, etc"""
+    """This function promts the user for the desired
+    time series. Day, week, month, etc"""
     while True:
         t_type = input('Please enter time series you would like to see. ' \
                   'Daily[1], Weekly[2], Monthly[3]: ')
@@ -30,11 +46,13 @@ def series():
             return t_type
         else:
             print('Error')
+            print(t_type)
             continue
     return t_type
 
 def time_series(t_series):
-    """this"""
+    """The time_series function takes the selected time series and return
+    the correct format for the url to retrive the symbol time series"""
     if t_series == 'Time Series (Daily)':
         api_series = '/query?function=TIME_SERIES_DAILY&symbol='
         return api_series
@@ -48,47 +66,60 @@ def time_series(t_series):
         print('Error') #DeBug
         print(t_series) #DeBug
 
-def company_symbol():
-    try:
-        #stock = ''
-        #while not stock:
-            #stock = input("What stock would you like to view? ")
-        stock = fetch_symbol(stock)
-        if len(stock) == 0:
-            print("Please enter a symbol.")
-        elif (stock) == 'Error Message':
-            print('else statment')
-    except requests.exceptions.ConnectionError:
-        print("Couldn't connect to server! Please check the network?")
-    return stock
+def date_price_df(fhand, t_series):
+    """The date_price_df function takes the file handle and creates a simple
+    data frame made up of the date and price"""
+    lst = []
+    for key, values in fhand.items():
+        if key == t_series:
+            for date, sinfo in values.items():
+                #Finds the date and values for stock
+                for cols, nums in sinfo.items():
+                    #finds the closing price
+                    if cols == '4. close':
+                        #create list for date and price key values
+                        lst.append([date,nums])
+    return lst
 
-t_series = series()
-api_root = 'https://www.alphavantage.co'
-api_series = time_series(t_series)
-api = 'Enter API code here'
-fhand = fetch_symbol()
-#print(fhand)
-#print(fhand[t_series]) #DeBug
-#print(t_series)
+def createDframe(fhand):
+    """This function finds the date and price for the selected stock
+    and creates and returns the dataframe"""
+    lst = []
+    for key, values in fhand.items():
+        if key == t_series:
+            for date, sinfo in values.items():
+                #Finds the date and values for stock
+                for cols, nums in sinfo.items():
+                    #finds the closing price
+                    if cols == '4. close':
+                        #create list for date and price key values
+                        lst.append([date,nums])
+    df = pd.DataFrame(lst,columns=['Date','Price'])
+    return df
 
-lst = []
+def createDframe2(fhand):
+    """This function finds the date and volume for the selected stock
+    and creates and returns the dataframe"""
+    lst = []
+    for key, values in fhand.items():
+        if key == t_series:
+            for date, sinfo in values.items():
+                #Finds the date and values for stock
+                for cols, vol in sinfo.items():
+                    #finds the volume
+                    if cols == '5. volume':
+                        #create list for date and volume key values
+                        lst.append([date,vol])
 
-for key, values in fhand.items():
-    if key == t_series:
-        for date, sinfo in values.items():
-            #Finds the date and values for stock
-            for cols, nums in sinfo.items():
-                #finds the closing price
-                if cols == '4. close':
-                    #create list for date and price key values
-                    lst.append([date,nums])
 
-#create series from list
-ds = pd.Series(lst)
-#create dataframe from list
-df = pd.DataFrame(lst,columns=['Date','Price'])
-#print(ds)
-print(df)
+    df = pd.DataFrame(lst,columns=['Date','Volume'])
+    return df
 
-plt.plot(df)
-plt.show()
+if __name__ == '__main__':
+    t_series = series()
+    api_root = 'https://www.alphavantage.co'
+    api_series = time_series(t_series)
+    api = #'API KEY GOES HERE'
+    fhand = company_symbol()
+    # Debug Print and test df
+    print(createDframe2(fhand))
